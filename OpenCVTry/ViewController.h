a917977875e6e7dbbfbbdac5182cfac1bf806f21
Kohei Arai <tobecomebig@gmail.com>
2014-09-16 12:35:29 -0400

    Initial Commit

---
+++ OpenCVTry/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OpenCVTry
+//
+//  Created by Kohei Arai on 9/16/14.
+//  Copyright (c) 2014 Kohei Arai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
